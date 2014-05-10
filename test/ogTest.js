5adf0039d468fc986031c95c2b4cddc130a2c254
Duncan Beaton <d@duncky.com>
2014-05-10 13:58:16 +0100

    add og using json; fix parsing in lib; add tests back

--- test/ogTest.js
+++ test/ogTest.js
@@ -7,68 +7,62 @@ grunt.config.init({});
 
 describe('og', function() {
 
-  var file,
-    lorem;
+    var file,
+        lorem;
 
-  beforeEach(function() {
-    // grunt.config.init();
-    // grunt.config('og', {
-    //   html: 'fixtures/index.html'
-    // });
-    // grunt.task.run('og');
-    // grunt.task.start();
-    file = grunt.file.read(path.join(__dirname, 'tmp/sample.html'));
-  });
+    beforeEach(function() {
+        file = grunt.file.read(path.join(__dirname, 'tmp/sample.html'));
+      });
 
-  describe('title', function() {
-    it('standard', function() {
-      expect(file).to.contain('<meta property="og:title" content="Page Title"/>');
+    describe('title', function() {
+        it('standard', function() {
+            expect(file).to.contain('<meta property="og:title" content="Page Title"/>');
+        });
+        it('google', function() {
+            expect(file).to.contain('<meta itemprop="name" content="Page Title">');
+        });
+        it('twitter', function() {
+            expect(file).to.contain('<meta name="twitter:title" content="Page Title">');
+        });
     });
-    //       it('google', function() {
-    //         expect(file).to.contain('<meta itemprop="name" content="Page Title">');
-    //       });
-    //       it('twitter', function() {
-    //         expect(file).to.contain('<meta name="twitter:title" content="Page Title">');
-    //       });
-    //   });
 
-    //   describe('type', function() {
-    //       it('standard', function() {
-    //         expect(file).to.contain('<meta property="og:type" content="Type of page"/>');
-    //       });
-    //   });
+    describe('type', function() {
+        it('standard', function() {
+            expect(file).to.contain('<meta property="og:type" content="Type of page"/>');
+        });
+    });
 
-    //   describe('url', function() {
-    //       it('standard', function() {
-    //         expect(file).to.contain('<meta property="og:url" content="http://www.example.com"/>');
-    //       });
-    //   });
+    describe('url', function() {
+        it('standard', function() {
+            expect(file).to.contain('<meta property="og:url" content="http://www.example.com"/>');
+        });
+    });
 
-    //   describe('image', function() {
-    //     it('standard', function() {
-    //       expect(file).to.contain('<meta property="og:image" content="http://example.com/image.jpg"/>');
-    //     });
-    //     it('google', function() {
-    //       expect(file).to.contain('<meta itemprop="image" content="http://example.com/image.jpg">');
-    //     });
-    //     it('twitter', function() {
-    //       expect(file).to.contain('<meta name="twitter:image:src" content="http://example.com/image.jpg">');
-    //     });
-    //   });
+    describe('image', function() {
+        it('standard', function() {
+            expect(file).to.contain('<meta property="og:image" content="http://example.com/image.jpg"/>');
+        });
+        it('google', function() {
+            expect(file).to.contain('<meta itemprop="image" content="http://example.com/image.jpg">');
+        });
+        it('twitter', function() {
+            expect(file).to.contain('<meta name="twitter:image:src" content="http://example.com/image.jpg">');
+        });
+    });
 
-    //   describe('description', function() {
-    //     beforeEach(function() {
-    //       lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quaerat, harum, recusandae veritatis iste distinctio autem nulla fugiat culpa laudantium quis ullam ipsam rerum accusamus vel! Voluptatum quam libero beatae!';
-    //     })
-    //     it('standard', function() {
-    //       expect(file).to.contain('<meta property="og:description" content="' + lorem + '"/>');
-    //     });
-    //     it('google', function() {
-    //       expect(file).to.contain('<meta itemprop="description" content="' + lorem + '"/>');
-    //     });
-    //     it('twitter', function() {
-    //       expect(file).to.contain('<meta name="twitter:description" content="' + lorem + '"/>');
-    //     });
-  });
+    describe('description', function() {
+        beforeEach(function() {
+            lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quaerat, harum, recusandae veritatis iste distinctio autem nulla fugiat culpa laudantium quis ullam ipsam rerum accusamus vel! Voluptatum quam libero beatae!';
+        })
+        it('standard', function() {
+            expect(file).to.contain('<meta property="og:description" content="' + lorem + '"/>');
+        });
+        it('google', function() {
+            expect(file).to.contain('<meta itemprop="description" content="' + lorem + '"/>');
+        });
+        it('twitter', function() {
+            expect(file).to.contain('<meta name="twitter:description" content="' + lorem + '"/>');
+        });
+    });
 
 });

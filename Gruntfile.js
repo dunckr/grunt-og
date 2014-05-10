a18c84c2e47418d09306a8015bb75947a63712c8
Duncan Beaton <d@duncky.com>
2014-05-10 12:26:24 +0100

    add lib; mv spec to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,29 @@
 'use strict';
-module.exports = function (grunt) {
-  grunt.initConfig({
-    mochacli: {
-      all: ['spec/*Spec.js']
-    }
-  });
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadTasks('tasks');
-  grunt.registerTask('default', ['mochacli']);
-  grunt.registerTask('test', 'default');
+module.exports = function(grunt) {
+    grunt.initConfig({
+        og: {
+          files: ['test/tmp/sample.html']
+        },
+        mochacli: {
+            all: ['test/*Test.js']
+        },
+        copy: {
+            tests: {
+                expand: true,
+                cwd: 'test/fixtures/',
+                src: '**',
+                dest: 'test/tmp/'
+            }
+        },
+        clean: {
+            tests: ['test/tmp']
+        }
+    });
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.loadTasks('tasks');
+    grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['clean', 'copy', 'og', 'mochacli']);
 };
--- tasks/og.js
+++ tasks/og.js
@@ -1,4 +1,17 @@
-module.exports = function (grunt) {
-  grunt.registerMultiTask('og', 'add open graph metadata', function() {
-  });
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.registerMultiTask('og', 'add open graph metadata', function() {
+
+        var OGProcess = require('../lib/ogprocess');
+
+        this.files.forEach( function( file ) {
+            file.src.forEach( function( src ) {
+                if ( grunt.file.isFile( src ) ) {
+                    var og = new OGProcess( { data: grunt.file.read( src ) } );
+                    grunt.file.write( src, og.output );
+                }
+            });
+        });
+    });
 };
